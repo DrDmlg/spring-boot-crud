cd0d58d5c4f3b12ebab3ca51fd7b8c4c48c00038
Dmitry Dorokhov <b27011971@inbox.ru>
2023-11-04 19:09:34 +0300

    Creating a Controller for application requests

---
+++ src/main/java/net/proselyte/crud/controller/UserController.java
@@ -0,0 +1,22 @@
+package net.proselyte.crud.controller;
+
+import net.proselyte.crud.model.User;
+import net.proselyte.crud.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    @GetMapping("/users")
+    public String findAll(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "user-list";
+    }
+}
